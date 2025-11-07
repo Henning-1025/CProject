d0902a53f51fe9fd5bba04ad4776745a6b0e9a4f
Henning-1025 <sthe082025@gmail.com>
2025-11-07 06:47:30 +0100

    Clean

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,14 +1,8 @@
 #include <iostream>
 
 int main() {
-    // Disable synchronization with C stdio for faster I/O
     std::ios_base::sync_with_stdio(false);
-    
-    // Untie cin from cout
     std::cin.tie(nullptr);
-    
-    // Print Hello World
     std::cout << "Hello World!" << '\n';
-    
     return 0;
 }
